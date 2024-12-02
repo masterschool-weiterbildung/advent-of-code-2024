9797dde7979a3b029d9dd5186c4c87c6a77a8df5
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-12-02 13:06:12 +0100

    Day 1

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import my_util
 
 
@@ -11,6 +9,16 @@ def main():
     part_one(str_output)
 
     part_two(str_output)
+
+
+def part_one(str_output):
+    list_one_sorted, list_two_sorted = get_sorted_list(str_output)
+
+    total = 0
+    for count in range(len(list_one_sorted)):
+        total += abs(
+            int(list_one_sorted[count]) - int(list_two_sorted[count]))
+    print(total)
 
 
 def part_two(str_output):
@@ -33,23 +41,9 @@ def get_lists(str_output):
     return list_one, list_two
 
 
-def part_one(str_output):
-    list_one_sorted, list_two_sorted = get_sorted_list(str_output)
-
-    total = 0
-    for count in range(len(list_one_sorted)):
-        total += abs(
-            int(list_one_sorted[count]) - int(list_two_sorted[count]))
-    print(total)
-
-
 def get_sorted_list(str_output):
-    list_one = []
-    list_two = []
-    for val in str_output:
-        lst = val.split();
-        list_one.append(lst[0])
-        list_two.append(lst[1])
+    list_one, list_two = get_lists(str_output)
+
     list_one_sorted = sorted(list_one)
     list_two_sorted = sorted(list_two)
     return list_one_sorted, list_two_sorted
